63456d30b875966bd0f0c67e871a135a472fd9ed
molnarbalazsnandor <molnarbalazsnandor@gmail.com>
2022-10-12 11:51:34 +0200

    vicces apiDate

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 console.log("loaded");
 
 const apiKey = "uJpGIKfiHk9wuvj0wfw7zO9z7W3rFnslH31HrwLZ";
-let apiDate = "2022-10-12";
+let apiDate = "2005-04-01";
 let fetchURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${apiDate}`;
 
 /* fetch(fetchURL).then(function (response) {
