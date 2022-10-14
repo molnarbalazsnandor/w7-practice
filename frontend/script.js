a41430411f878673c12422b6d1207c9e6fa5beda
molnarbalazsnandor <molnarbalazsnandor@gmail.com>
2022-10-14 10:03:05 +0200

    c

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,11 @@
-fetch("https://swapi.dev/api/people");
+fetch("https://swapi.dev/api/people")
+  .then((response) => response.json())
+  .then((people) => console.log(people));
+
+//magyarázat: hívja meg az URL-t, then: várja meg a míg betölt, majd alakítsa át json ból js-es struktúrává majd abból kikérjük a people-t
+
+/* const response = await fetch("https://swapi.dev/api/people");
+const people = await respone.json;
+console.log(people) 
+
+ezek ekvivalens megoldások*/
